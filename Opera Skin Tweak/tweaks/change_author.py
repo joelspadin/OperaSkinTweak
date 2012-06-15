e236b0e51030302ed10a588f7208e7a0fba69d62
Joel Spadin <joelspadin@gmail.com>
2012-06-14 23:49:28 -0500

    Removed old Junk

--- Opera Skin Tweak/tweaks/change_author.py
+++
@@ -1,9 +0,0 @@
-
-name = 'Change skin author'
-
-def run(tweakdir, workingdir, ini):
-	ini['Info']['Author'] = 'Joel Spadin'
-	ini['Options']['Fallback foreground'] = '1'
-	ini['Options']['Fallback background'] = '1'
-
-	
